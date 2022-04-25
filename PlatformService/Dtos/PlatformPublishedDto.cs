2b14f03014b31d22d6e56318e39b52ee671d66d9
Jacob Tate <jacob.tate@digipen.edu>
2022-04-24 23:46:15 -0700

    Added Platform RabbitMQ Publisher

--- PlatformService/Dtos/PlatformPublishedDto.cs
+++ PlatformService/Dtos/PlatformPublishedDto.cs
@@ -3,7 +3,7 @@ namespace PlatformService.Dtos
     public class PlatformPublishedDto
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Event { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Event { get; set; } = string.Empty;
     }
 }
